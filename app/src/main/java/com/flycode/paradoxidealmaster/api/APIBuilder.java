bd53dbb2ed6c930bc7f4e95a51f69f535e584704
AnhaytAnanun <anhaytananun@gmail.com>
2016-09-20 14:40:44 +0400

    back to test server

--- app/src/main/java/com/flycode/paradoxidealmaster/api/APIBuilder.java
+++ app/src/main/java/com/flycode/paradoxidealmaster/api/APIBuilder.java
@@ -27,8 +27,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class APIBuilder {
 //    private static String BASE_URL = "http://192.168.0.111:1999";
 //    private static String BASE_URL = "http://192.168.0.107:1994";
-    private static String BASE_URL = "http://192.168.0.112:1994";
-//    private static String BASE_URL = "http://fly.co.de:1994";
+//    private static String BASE_URL = "http://192.168.0.112:1994";
+    private static String BASE_URL = "http://fly.co.de:1994";
     private static APIService idealAPI;
 
     public static APIService getIdealAPI() {
